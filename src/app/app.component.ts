1c5dc10627c304f3f3759f58c91565dded3abe3f
OzerOzyurt <ozersimav@gmail.com>
2024-11-13 11:46:24 +0300

    approutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
-import { MaincontentComponent } from '../components/maincontent/maincontent.component';
-import { SliderComponent } from '../components/slider/slider.component';
-import { NavbarBottomComponent } from '../components/navbar-bottom/navbar-bottom.component';
-import { SliderowlComponent } from '../components/sliderowl/sliderowl.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,MaincontentComponent,SliderComponent,NavbarBottomComponent,SliderowlComponent],
+  imports: [RouterOutlet,NavbarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: HomeComponent
+    },
+
+    {
+        path:'hakkımızda' , loadComponent: () => import ('../components/about/about.component').then((m)=>m.AboutComponent)
+    },
+    {
+        path: '404',
+        loadComponent: () =>
+          import('../components/notfound/notfound.component').then(
+            (m) => m.NotfoundComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: '/404',
+      },
+];
